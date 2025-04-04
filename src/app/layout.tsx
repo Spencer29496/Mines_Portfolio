a83a96c0ea1a98a9edad8c8cc64be5f29b4ea47b
Spencer29496 <spencer.mines@yahoo.com>
2025-04-04 17:32:39 -0400

    fixed tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,16 +34,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-TYY929FN5Y"
-          strategy="afterInteractive"
-        />
-        <Script id="gtag-init" strategy="afterInteractive">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-TYY929FN5Y" />
+        <Script id="gtag-init">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -51,6 +45,11 @@ export default function RootLayout({
             gtag('config', 'G-TYY929FN5Y');
           `}
         </Script>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
         {children}
         <Analytics />
       </body>
