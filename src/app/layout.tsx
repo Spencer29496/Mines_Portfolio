a1f938031f4033d4b329a1b9721314c6fb6435f7
Spencer29496 <spencer.mines@yahoo.com>
2025-04-04 17:43:14 -0400

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Script from "next/script";
 import "./globals.css";
 
@@ -54,6 +55,7 @@ export default function RootLayout({
         </Script>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
